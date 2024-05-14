c472a816daa2a9fe1d87e76f640ab552956929ef
jsalvadormasip <127728014+jsalvadormasip@users.noreply.github.com>
2024-05-14 16:25:16 +0200

    simplethrustpowerandtorque

--- thrust_power_torque.py
+++ thrust_power_torque.py
@@ -2,7 +2,26 @@
 
 import numpy as np
 
-import circlesinsqure as c
+
+
+rho = 1.225 #SI units
+
+nrotors = 33
+radius = 170/((nrotors*(1+0))**0.5) #si units
+Uinf = 10 #si units
+Power = 30 *10**6 #Watts
+assumed_a = 0.4
+Area = np.pi*radius**2*nrotors
+
+thrustperrotor = Power/Uinf/(1-assumed_a)/nrotors
+print("Thrust per rotor is ", thrustperrotor)
+
+TSR = 8
+omega =  Uinf*TSR/radius
+Torqueperrotor = Power / omega/nrotors
+print("Torque per rotor is ", Torqueperrotor)
+
+
 def CTfunction(a, glauert = False):
     """
     This function calculates the thrust coefficient as a function of induction factor 'a'
@@ -10,10 +29,10 @@ def CTfunction(a, glauert = False):
     """
     CT = np.zeros(np.shape(a))
     CT = 4*a*(1-a)  
-    # if glauert:
-    #     CT1=1.816;
-    #     a1=1-np.sqrt(CT1)/2;
-    #     CT[a>a1] = CT1-4*(np.sqrt(CT1)-1)*(1-a[a>a1])
+    if glauert:
+        CT1=1.816;
+        a1=1-np.sqrt(CT1)/2;
+        CT[a>a1] = CT1-4*(np.sqrt(CT1)-1)*(1-a[a>a1])
     
     return CT
   
@@ -29,20 +48,17 @@ def ainduction(CT):
     a[CT>=CT2] = 1 + (CT[CT>=CT2]-CT1)/(4*(np.sqrt(CT1)-1))
     a[CT<CT2] = 0.5-0.5*np.sqrt(1-CT[CT<CT2])
     return a
+a = np.arange(-.5,1,.01)
+CTmom = CTfunction(a) # CT without correction
+CTglauert = CTfunction(a, True) # CT with Glauert's correction
+a2 = ainduction(CTglauert)
 
-rho = 1.225 #SI units
-radius = c.radius1 #si units
-nrotors = c.n
-Uinf = 9 #si units
-Power = 0.631145 *10**6 #Watts
-assumed_a = 0.5
-Area = np.pi*radius**2*nrotors
-CT = CTfunction(assumed_a)
-thrust1 = CT*0.5*rho*Uinf**2*Area
-thrust2 = Power/Uinf/(1-assumed_a)
-print(thrust1, thrust2)
-
-TSR = 8
-omega =  Uinf*TSR/radius
-Torque = Power / omega
-print("Torque is ", Torque)
+fig1 = plt.figure(figsize=(12, 6))
+plt.plot(a, CTmom, 'k-', label='$C_T$')
+plt.plot(a, CTglauert, 'b--', label='$C_T$ Glauert')
+plt.plot(a, CTglauert*(1-a), 'g--', label='$C_P$ Glauert')
+plt.xlabel('a')
+plt.ylabel(r'$C_T$ and $C_P$')
+plt.grid()
+plt.legend()
+plt.show()
