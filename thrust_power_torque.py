f539ad91fab2bcf08354d2a20cf85898e1c518bb
5546230 <n.g.j.schotmeijer@student.tudelft.nl>
2024-05-21 12:20:33 +0200

    add torque curves and update plots

--- thrust_power_torque.py
+++ thrust_power_torque.py
@@ -14,6 +14,7 @@ radius = magic_radius_number_providedbyTiago/((nrotors*(1+0))**0.5) #si units
 Uinf = 9.6 #si units
 Power = 30 *10**6 #Watts
 TSR = 8
+rated = 9.7 # [m/s]
 # \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
 
@@ -43,6 +44,42 @@ def ainduction(CT):
     a[CT>=CT2] = 1 + (CT[CT>=CT2]-CT1)/(4*(np.sqrt(CT1)-1))
     a[CT<CT2] = 0.5-0.5*np.sqrt(1-CT[CT<CT2])
     return a
+
+
+def beta_formula(wind_speed: float, rotational_speed: float, radius: float, rated_speed: float = 12) -> float:
+    '''
+    Calculates the pitch angle beta in radians based on the wind speed and rotational speed.
+    Assumes the pitch angle is zero at a wind speed equal to the rated speed.
+    ---
+    Params:
+    ----
+    - wind_speed: the wind_speed in m/s.
+    - rotational_speed: the rotational speed in rad/s.
+    - rated_speed: The rated speed in m/s. Defaults to 12 m/s.
+
+    ---
+    Outputs:
+    ---
+    - beta: pitch angle in rad.
+    '''
+    
+    phi = np.arctan(rotational_speed * radius/ wind_speed)
+    beta = phi - np.arctan(rotational_speed * radius/ rated_speed)
+    return -beta
+
+def cp_formula(tsr: float, beta: float) -> float:
+    '''
+    Calculates the cp based on the tsr and beta
+    '''
+    c1, c2, c3, c4, c5, c6 = 0.5176, 116, 0.4, 5, 21, 0.0068
+
+    lambda_inv = 1/(tsr + 0.08* beta) - 0.035/(beta**3 + 1)
+    tsr_i = 1/lambda_inv
+
+    cp = c1 * (c2/tsr_i - c3*beta - c4) * np.exp(-c5/tsr_i) + c6*tsr
+    return cp
+
+omega = rated * TSR / radius 
 a = np.arange(-.5,1,.01)
 CTmom = CTfunction(a) # CT without correction
 CTglauert = CTfunction(a, True) # CT with Glauert's correction
@@ -70,32 +107,36 @@ if CP> np.max(CTglauert*(1-a)):
 
 Uinf_graph = np.arange(0, 30, 0.01)
 Power_graph_lst = []
-# Torque_graph_lst = []
+Torque_graph_lst = []
 for Uinfinity in Uinf_graph:
     Power_graph = 30*10**6
     CP_graph = Power_graph/nrotors/(0.5*rho*Uinfinity**3*np.pi*radius**2)  #necessary CP.
     
     if CP_graph> np.max(CTglauert*(1-a)):
         CP_graph = min(CP, np.max(CTglauert*(1-a)))
-        Power_graph = CP*(0.5*rho*Uinfinity**3*np.pi*radius**2)*nrotors
+        # CP_graph = cp_formula(tsr, beta)
+    Power_graph = CP_graph*(0.5*rho*Uinfinity**3*np.pi*radius**2)*nrotors
         
+    tsr = omega * radius / Uinfinity
+    Torque_graph = CP_graph / tsr * 0.5 * rho * Uinfinity ** 2 * np.pi * radius ** 3
+
     if Uinfinity > 25 or Uinfinity <3:   #cut in and out speed
         Power_graph = 0
         Torque_graph = 0
     # omega_graph =  Uinfinity*TSR/radius
-    # Torque_graph = Power_graph / omega_graph
-    Power_graph_lst.append(Power_graph)
-    # Torque_graph_lst.append(Torque_graph)
+    # Torque_graph = Power_graph / omega
+    Power_graph_lst.append(Power_graph*1e-6)
+    Torque_graph_lst.append(Torque_graph*1e-3)
 
 Power_graph_lst = np.array(Power_graph_lst)
 # Torque_graph_lst = np.array(Torque_graph_lst)
 
-fig2 = plt.figure(figsize=(12, 6))
-plt.plot(Uinf_graph, Power_graph_lst)
-plt.xlabel('Wind Speed')
-plt.ylabel('Power Generated')
-plt.grid()
-plt.show()
+# fig2 = plt.figure(figsize=(12, 6))
+# plt.plot(Uinf_graph, Power_graph_lst)
+# plt.xlabel('Wind Speed')
+# plt.ylabel('Power Generated')
+# plt.grid()
+# plt.show()
 
 # fig3 = plt.figure(figsize=(12, 6))
 # plt.plot(Uinf_graph, Torque_graph_lst)
@@ -103,6 +144,27 @@ plt.show()
 # plt.ylabel('Torque Generated')
 # plt.grid()
 # plt.show()
+
+fig, axs = plt.subplots(1, 2)
+fig.set_figheight(3)
+fig.set_figwidth(10)
+
+axs[0].plot(Uinf_graph, Power_graph_lst)
+axs[0].set_xlabel('Wind Speed [m/s]')
+axs[0].set_xlim(0,30)
+axs[0].set_ylabel('Power Generated [GW]')
+axs[0].grid(True)
+
+axs[1].plot(Uinf_graph, Torque_graph_lst)
+axs[1].set_xlabel('Wind Speed [m/s]')
+axs[1].set_xlim(0,30)
+axs[1].set_ylabel('Torque per Rotor [kNm]')
+axs[1].grid(True)
+
+plt.tight_layout()
+plt.savefig('power_torque_curves.svg', format='svg')
+plt.show()
+
 def find_all_x_crossings(x_data, y_data, y_targets):
     crossings = {y: [] for y in y_targets}
 
