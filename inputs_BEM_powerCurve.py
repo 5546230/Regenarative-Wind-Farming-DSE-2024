70a73a7ea81f069056485f677d4d584d54dd67b6
jsalvadormasip <127728014+jsalvadormasip@users.noreply.github.com>
2024-05-30 11:49:59 +0200

    1

--- inputs_BEM_powerCurve.py
+++ inputs_BEM_powerCurve.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-V_RATED = 10
+V_RATED = 17.5
 TSR = 8
 P_RATED = 30*10**6
 n_rotors = 33
@@ -8,8 +8,8 @@ rho = 1.225
 cut_in = 3 # [m/s]
 cut_off = 25 # [m/s]
 pitch = 2 #degrees  #tip pitch i think
-iteration = True
-init_Radius = 31
+iteration = False
+init_Radius = 21
 tip_chord = 1
 root_chord = 3
 root_twist = -14
