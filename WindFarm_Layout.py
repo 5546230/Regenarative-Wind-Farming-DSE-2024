9aeccb0d2de14a9425fd99899fb1b7d6cabfc28f
tcoelho30 <tiago_coelho30@hotmail.com>
2024-06-03 17:30:00 +0200

    Fixed Most of the Bugs
    It actually works now yay, well sorta

--- WindFarm_Layout.py
+++ WindFarm_Layout.py
@@ -1,5 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import math
+from scipy.spatial.distance import cdist
+
 
 def create_parallelogram(vertices):
     """
@@ -40,12 +43,32 @@ def distance_point_to_line(point, line_start, line_end):
     Returns:
     float: Perpendicular distance from the point to the line
     """
+    # px, py = point
+    # x1, y1 = line_start
+    # x2, y2 = line_end
+    # num = abs((y2 - y1) * px - (x2 - x1) * py + x2 * y1 - y2 * x1)
+    # den = np.sqrt((y2 - y1)**2 + (x2 - x1)**2)
+    
     px, py = point
+    angle = 65.3
+    # Defining equation of first line y = dx + e
+    d = np.tan(np.radians(angle))
+    e = py - d * px
+
+    # Defining equation of second line y =mx + c
     x1, y1 = line_start
     x2, y2 = line_end
-    num = abs((y2 - y1) * px - (x2 - x1) * py + x2 * y1 - y2 * x1)
-    den = np.sqrt((y2 - y1)**2 + (x2 - x1)**2)
-    return num / den
+    
+    m = (y2 -y1)/(x2-x1)
+    c = y1 - m *x1
+
+    # Equating both lines - dx + e = mx + c
+    x = (c-e)/(d-m)
+    y = m*x +c
+    final = [ x, y ]
+    distance = math.dist(point, final)
+
+    return distance
 
 def generate_points(parallelogram, t_dist):
     """
@@ -63,6 +86,8 @@ def generate_points(parallelogram, t_dist):
 
     A, B, C, D = parallelogram
 
+    NbrPoints = 0
+
     # Calculate FirstColPoints
     distance_AB = np.linalg.norm(B - A)
     FirstColPoints = int(distance_AB // t_dist)
@@ -72,12 +97,14 @@ def generate_points(parallelogram, t_dist):
     FirstRowPoints = int(distance_AD // t_dist)
 
     # Generate points along the line A-B
-    for i in range(FirstColPoints):
-        t = i / (FirstColPoints - 1)
+    for i in range(FirstColPoints +1):
+        t = i / (FirstColPoints)
         x = (1 - t) * A[0] + t * B[0]
         y = (1 - t) * A[1] + t * B[1]
         x_coords.append(x)
         y_coords.append(y)
+
+        NbrPoints += 1
 
     # Generate points for the next columns
     ChangeinY = np.tan(np.radians(-19)) * t_dist
@@ -85,8 +112,9 @@ def generate_points(parallelogram, t_dist):
 
     counter = 1 # Counting number of points in column
     next = 0 # Switch to decrease number of points
+
     for j in range(2,FirstRowPoints+1):
-        print("Calculating col. ", j)
+        # print("Calculating col. ", j)
         # Testing if we decrease the amount of points in the column
         if j % NumberToNew == 0:
             # WE DECREASE ON THE NEXT
@@ -96,12 +124,26 @@ def generate_points(parallelogram, t_dist):
             # Calculate the distance from the first point of the column to the line B-C
             first_point_column = (A[0] + (j-1)*t_dist, A[1])
             distance_to_BC = distance_point_to_line(first_point_column, B, C)
-            for i in range(FirstColPoints - counter):
-                t = i / (FirstColPoints - 1)
-                x = x_coords[i] + (j-1) * t_dist
+            for i in range(FirstColPoints - counter +1):
+                # t = i / (FirstColPoints - 1)
+                # x = x_coords[i] + (j-1) * t_dist
 
-                y_spacing = distance_to_BC/(FirstRowPoints - (j-1))
-                y =  A[1] + (i) * y_spacing 
+                # y_spacing = distance_to_BC/(FirstRowPoints - counter)
+                # y =  A[1] + (i) * y_spacing 
+
+                if i == 0: # Defining first point of each col
+                    x = A[0] + (j-1) * t_dist
+                    y = A[1]
+
+                else:
+                    # y_spacing = distance_to_BC/(FirstRowPoints - counter)
+                    # y =  A[1] + (i) * y_spacing 
+                    
+                    y_spacing = distance_to_BC/(FirstColPoints - counter)
+                    
+                    x = (j-1) * t_dist + i * np.cos(np.radians(65.3)) * (y_spacing)
+
+                    y =  np.sin(np.radians(65.3)) * (y_spacing * (i))
 
                 if is_point_in_parallelogram((x, y), parallelogram):
                     x_coords.append(x)
@@ -113,12 +155,22 @@ def generate_points(parallelogram, t_dist):
             # Calculate the distance from the first point of the column to the line B-C
             first_point_column = (A[0] + (j-1)*t_dist, A[1])
             distance_to_BC = distance_point_to_line(first_point_column, B, C)
-            for i in range(FirstColPoints  - counter ):
-                t = i / (FirstColPoints - 1)
-                x = x_coords[i] + (j-1) * t_dist
+            for i in range(FirstColPoints  - counter + 1):
+                # t = i / (FirstColPoints - 1)
 
-                y_spacing = distance_to_BC/(FirstRowPoints - (j-1))
-                y =  A[1] + (i) * y_spacing 
+                if i == 0: # Defining first point of each col
+                    x = (j-1) * t_dist
+                    y = 0
+
+                else:
+                    # y_spacing = distance_to_BC/(FirstRowPoints - counter)
+                    # y =  A[1] + (i) * y_spacing 
+                    
+                    y_spacing = distance_to_BC/(FirstColPoints - counter)
+
+                    x = (j-1) * t_dist + i * np.cos(np.radians(65.3)) * (y_spacing)
+
+                    y =  np.sin(np.radians(65.3)) * (y_spacing * (i))
 
                 if is_point_in_parallelogram((x, y), parallelogram):
                     x_coords.append(x)
@@ -132,21 +184,47 @@ def generate_points(parallelogram, t_dist):
             # Calculate the distance from the first point of the column to the line B-C
             first_point_column = (A[0] + (j-1)*t_dist, A[1])
             distance_to_BC = distance_point_to_line(first_point_column, B, C)
-            for i in range(FirstColPoints  - counter ):
-                t = i / (FirstColPoints - 1)
-                x = x_coords[i] + (j-1) * t_dist
+            for i in range(FirstColPoints  - counter +1):
+                # t = i / (FirstColPoints - 1)
+                # x = x_coords[i] + (j-1) * t_dist
+                if i == 0: # Defining first point of each col
+                    x = (j-1) * t_dist
+                    y = 0
 
-                y_spacing = distance_to_BC/(FirstRowPoints - (j-1))
-                y =  A[1] + (i) * y_spacing 
-                
+                else:
+                    # y_spacing = distance_to_BC/(FirstRowPoints - counter)
+                    # y =  A[1] + (i) * y_spacing 
+                    
+                    y_spacing = distance_to_BC/(FirstColPoints - counter)
+                    
+                    x = (j-1) * t_dist + i * np.cos(np.radians(65.3)) * (y_spacing)
+
+                    y =  np.sin(np.radians(65.3)) * (y_spacing * (i))
+
+
                 if is_point_in_parallelogram((x, y), parallelogram):
                     x_coords.append(x)
                     y_coords.append(y)
+        NbrPoints = NbrPoints + i
+
+    # Calculate FirstColPoints
+    distance_CD = np.linalg.norm(C - D)
+    LastColPoints = int(distance_CD // t_dist)
+
+    # Generate points along the line A-B
+    for i in range(LastColPoints):
+        t = i / (LastColPoints - 1)
+        x = (1 - t) * C[0] + t * D[0]
+        y = (1 - t) * C[1] + t * D[1]
+        x_coords.append(x)
+        y_coords.append(y)
+
+        NbrPoints += 1
+    
+    print("Total Points: ", NbrPoints)
 
 
-
-
-    return x_coords, y_coords, FirstColPoints, FirstRowPoints, distance_to_BC
+    return x_coords, y_coords, FirstColPoints, FirstRowPoints, distance_to_BC, NbrPoints
 
 def plot_parallelogram_and_points(parallelogram, x_coords, y_coords):
     """
@@ -172,6 +250,61 @@ def plot_parallelogram_and_points(parallelogram, x_coords, y_coords):
     plt.axvline(0, color='black', linewidth=0.5)
     plt.show()
 
+
+# def filter_points(x_coords, y_coords, t_dist):
+#     """
+#     Filter points to remove those that are closer than t_dist to the previous point.
+
+#     Parameters:
+#     x_coords (list): x coordinates of the points
+#     y_coords (list): y coordinates of the points
+#     t_dist (float): Minimum distance between points
+
+#     Returns:
+#     tuple: Filtered x and y coordinates
+#     """
+#     filtered_x_coords = []
+#     filtered_y_coords = []
+
+#     for i in range(0, len(x_coords)):
+#         final = [x_coords[i],y_coords[i]]
+#         point = [x_coords[i-1],y_coords[i-1]]
+
+
+#         distance = np.abs(math.dist(point, final))
+#         # distance = np.sqrt((x_coords[i] - x_coords[i-1])**2 + (y_coords[i] - y_coords[i-1])**2)
+#         if distance >= t_dist:
+#             filtered_x_coords.append(x_coords[i])
+#             filtered_y_coords.append(y_coords[i])
+
+#     return filtered_x_coords, filtered_y_coords
+
+
+def filter_points(x_coords, y_coords, t_dist):
+    """
+    Filter points to remove those that are closer than t_dist to the previous point.
+
+    Parameters:
+    x_coords (list): x coordinates of the points
+    y_coords (list): y coordinates of the points
+    t_dist (float): Minimum distance between points
+
+    Returns:
+    tuple: Filtered x and y coordinates
+    """
+    coords = [x_coords,y_coords]
+
+    coords = np.column_stack((x_coords, y_coords))
+
+    for i in range(0, len(x_coords)):
+        point = np.array([x_coords[i],y_coords[i]])   
+        coords[(cdist(coords[:,:],point[None]) > t_dist).ravel()]
+    
+    filtered_x_coords = coords[:, 0]
+    filtered_y_coords = coords[:, 1]
+    return filtered_x_coords, filtered_y_coords
+
+
 # Define the vertices of the parallelogram based on given dimensions and angles
 A = np.array([0, 0])
 B = np.array([27.5 * np.cos(np.radians(65.3)), 27.5 * np.sin(np.radians(65.3))])
@@ -183,7 +316,12 @@ parallelogram = create_parallelogram([A, B, C, D])
 t_dist = float(input("Enter the value for t_dist: "))
 
 # Generate points
-x_coords, y_coords, FirstColPoints, FirstRowPoints, distance_to_BC = generate_points(parallelogram, t_dist)
+x_coords, y_coords, FirstColPoints, FirstRowPoints, distance_to_BC, NbrPoints = generate_points(parallelogram, t_dist)
+
+
+# Filter points based on the distance
+filtered_x_coords, filtered_y_coords = filter_points(x_coords, y_coords, t_dist)
+
 
 # Print the number of points
 print(f"FirstColPoints: {FirstColPoints}")
@@ -191,5 +329,7 @@ print(f"FirstRowPoints: {FirstRowPoints}")
 print(f"Distance from the first point of the second column to the line B-C: {distance_to_BC}")
 
 # Plot the parallelogram and the points within it
-plot_parallelogram_and_points(parallelogram, x_coords, y_coords)
+# plot_parallelogram_and_points(parallelogram, x_coords, y_coords)
 
+# Plot the parallelogram and the points within it
+plot_parallelogram_and_points(parallelogram, filtered_x_coords, filtered_y_coords)
