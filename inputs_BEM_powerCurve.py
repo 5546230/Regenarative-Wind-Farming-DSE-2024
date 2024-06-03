4a9977fa9e315c8ad4b33c29149ba3655b13c011
jsalvadormasip <127728014+jsalvadormasip@users.noreply.github.com>
2024-06-03 12:33:58 +0200

    optimized blade geometry

--- inputs_BEM_powerCurve.py
+++ inputs_BEM_powerCurve.py
@@ -1,19 +1,19 @@
 import numpy as np
 
-V_RATED = 13.5  #13.6 is effectively the biggest for 33 rotors
+V_RATED = 13.48  #13.6 is effectively the biggest for 33 rotors
 TSR = 8
 P_RATED = 30*10**6
 n_rotors = 33
 rho = 1.225
 cut_in = 3 # [m/s]
 cut_off = 25 # [m/s]
-pitch = -2.70647525#2 #degrees  #tip pitch i think
+pitch = -3.275370556176349#2 #degrees  #tip pitch i think
 iteration = True     #iterates to converge for the correct radius. 
 init_Radius = 19.89 #initial radius estimate. If you don't trust it, set automatic_radius to True, which estimates initial radius based on assumed_CP
 automatic_radius = True   
 tip_chord = 1
 root_chord = 2#3
-root_twist = -8.30282242#-14
+root_twist = -8.420070544909368#-14
 optimize = False    #optimize blade geometry (pitch, twist, chords)
 assumed_CP = 0.46  
 ale_shit = False   #something bc alessandro wanted CT(TSR, pitch)
