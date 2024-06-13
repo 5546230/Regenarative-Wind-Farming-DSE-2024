8df3f4af7b3eac26b72920ea5e07708fba97a169
jsalvadormasip <127728014+jsalvadormasip@users.noreply.github.com>
2024-06-13 12:50:43 +0200

    1

---
+++ estimateCapacityFactor.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+number_turbines = np.array([15,14,14,14,14,13,13,13,13,12,12,12,12,11,11,11,11,10,10,10,10,9,9,9,6])
+assumed_regeneration = 0.95
+
+
+
+# print(np.sum(number_turbines))
+# energy = 1*assumed_regeneration**(number_turbines[-1])
+# print(energy)
