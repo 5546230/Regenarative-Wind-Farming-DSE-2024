376446828535c1ec6a863d02caa6cbbb186335df
jsalvadormasip <127728014+jsalvadormasip@users.noreply.github.com>
2024-06-18 15:18:40 +0200

    -

--- estimateCapacityFactor.py
+++ estimateCapacityFactor.py
@@ -2,7 +2,7 @@ import numpy as np
 import generatorselection as g
 
 number_turbines = np.array([15,14,14,14,14,13,13,13,13,12,12,12,12,11,11,11,11,10,10,10,10,9,9,9,6])
-assumed_regeneration = 0.912 #0.95 would be ideal but someone chose the wrong number of wings. 
+assumed_regeneration = 0.928 #0.95 would be ideal but someone chose the wrong number of wings. 
 assumed_regeneration_speed = assumed_regeneration**(1/3)
 
 unit_cf = 0.59 
